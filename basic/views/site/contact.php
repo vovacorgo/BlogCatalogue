6bff10a4522802fc69719b8802b0d28fe3418eea
Volodymyr <47760619+vovacorgo@users.noreply.github.com>
2022-10-27 09:36:41 +0300

    Testing of git status

--- basic/views/site/contact.php
+++ basic/views/site/contact.php
@@ -16,9 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
 
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
+     
 
         <p>
             Note that if you turn on the Yii debugger, you should be able
